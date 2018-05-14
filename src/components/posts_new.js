9ec84d7a79a185f287b194dd12588fbabfcd021d
JimiJonJimbo <jimmyling3@gmail.com>
2018-05-14 12:19:48 -0700

    Finished lecture 143

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -48,7 +48,9 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
